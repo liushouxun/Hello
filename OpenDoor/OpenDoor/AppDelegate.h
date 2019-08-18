7e66154b298ea6f9ad5026d13f1a7cd452c6f563
liusx <952520223@qq.com>
2019-08-18 21:54:22 +0800

    新增项目

---
+++ OpenDoor/OpenDoor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenDoor
+//
+//  Created by 刘守勋 on 2019/8/18.
+//  Copyright © 2019 刘守勋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenDoor/OpenDoor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenDoor
+//
+//  Created by 刘守勋 on 2019/8/18.
+//  Copyright © 2019 刘守勋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
